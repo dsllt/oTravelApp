864c9e04ce20bf733bc48db01d02975951febd4a
dsllt <dayanesallet@hotmail.com>
2024-10-31 20:53:24 -0300

    feat: creates home header

---
+++ src/screens/app/HomeScreen/components/HomeHeader.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {SimpleLogo} from '@brand';
+
+import {Box, BoxProps, Icon} from '@components';
+
+export function HomeHeader() {
+  return (
+    <Box {...$wrapper}>
+      <SimpleLogo width={70} />
+      <Box flexDirection="row" gap="s24">
+        <Icon name="search" />
+        <Icon name="bell" />
+        <Icon name="message" />
+      </Box>
+    </Box>
+  );
+}
+
+const $wrapper: BoxProps = {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingHorizontal: 's24',
+  paddingBottom: 's24',
+};
